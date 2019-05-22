2903a8fd91e93eb105da8d17bf4156e7fb125f7f
Emily Creamer <emily.creamer@icloud.com>
2019-05-22 15:49:27 -0700

    lunchActions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { ADD_DRINK, ADD_SANDWICH, ADD_CHIPS, REMOVE_DRINK, REMOVE_SANDWICH, REMOVE_CHIPS} from './actions/lunchActions';
 
 const initialState = {
   drink: null,
@@ -8,16 +9,18 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch(action.type) {
-    case 'ADD_DRINK':
+    case ADD_DRINK:
       return { ...state, drink: action.payload };
-    case 'ADD_SANDWICH':
+    case ADD_SANDWICH:
       return { ...state, sandwich: action.payload };
-    case 'ADD_CHIPS':
+    case ADD_CHIPS:
       return { ...state, chips: action.payload };
-    case 'REMOVE_DRINK':
+    case REMOVE_DRINK:
       return { ...state, drink: null };
-    case 'REMOVE_SANDWICH':
-      return { ...state, chips: null };
+    case REMOVE_SANDWICH:
+      return { ...state, sandwich: null };
+    case REMOVE_CHIPS:
+      return { ...state, chips: null};
     default:
       return state;
   }
@@ -26,42 +29,42 @@ function reducer(state = initialState, action) {
 const store = createStore(reducer);
 
 store.dispatch({
-  type: 'ADD_DRINK',
+  type: ADD_DRINK,
   payload: 'Iced Tea'
 });
 
 console.log('Added your drink.', store.getState());
 
 store.dispatch({
-  type: 'ADD_SANDWICH',
+  type: ADD_SANDWICH,
   payload: 'Veggie + Hummus'
 });
 
 console.log('Added your sandwich.', store.getState());
 
 store.dispatch({
-  type: 'ADD_CHIPS',
+  type: ADD_CHIPS,
   payload: 'Pop Chips'
 });
 
 console.log('Added your chips.', store.getState());
 
 store.dispatch({
-  type: 'REMOVE_DRINK',
+  type: REMOVE_DRINK,
   payload: 'Iced Tea'
 });
 
 console.log('Removed your drinks.', store.getState());
 
 store.dispatch({
-  type: 'REMOVE_SANDWICH',
+  type: REMOVE_SANDWICH,
   payload: 'Veggie + Hummus'
 });
 
 console.log('Removed your sandwich.', store.getState());
 
 store.dispatch({
-  type: 'REMOVE_CHIPS',
+  type: REMOVE_CHIPS,
   payload: 'Pop Chips'
 });
 
