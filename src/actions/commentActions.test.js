69f40ade5453fba337c34443c5907c384813a708
Emily Creamer <emily.creamer@icloud.com>
2019-05-23 10:22:05 -0700

    commentActions tests PASSing

---
+++ src/actions/commentActions.test.js
@@ -0,0 +1,24 @@
+import {
+  createComment,
+  CREATE_COMMENT,
+  deleteComment,
+  DELETE_COMMENT,
+  updateComment,
+  UPDATE_COMMENT
+} from './commentActions';
+
+describe('Comment Action', () => {
+  it('Creates a CREATE_COMMENT', () => {
+    expect(createComment(0, 'My comment.')).toEqual({
+      type: CREATE_COMMENT,
+      payload: { postId: 0, comment: 'My comment.' }
+    });
+  });
+
+  it('Creates a DELETE_COMMENT', () => {
+    expect(deleteComment(0, 1)).toEqual({
+      type: DELETE_COMMENT,
+      payload: { postId: 0, commentId: 1 }
+    });
+  });
+})
